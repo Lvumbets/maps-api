573fea514c18e14fc0b1b76d9a08952e51db160d
user <andrey.d.salomatin@gmail.com>
2025-02-28 20:31:52 +0300

    themes commit

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 
 delta = 0.005
-
+theme = 'light'
 
 def get_response():
     global delta
     map_params = {
         "ll": "37.3,55.48",
+        "theme": theme,
         "spn": ','.join(map(str, [delta, delta])),
         "apikey": 'a235da75-91e6-4389-8a53-60346aa1414e',
     }
@@ -38,7 +39,7 @@ def render():
 
 def run():
     global delta
-
+    global theme
     running = True
     while running:  # запуск основного цикла
         for event in pygame.event.get():
@@ -46,10 +47,15 @@ def run():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    theme = 'light'
+                if event.key == pygame.K_2:
+                    theme = 'dark'
                 if event.key == pygame.K_PAGEUP and delta < 3:
                     delta += 0.1
                 if event.key == pygame.K_PAGEDOWN and delta - 0.1 > 0:
                     delta -= 0.1
+
         # отображение ресурсов на экране
         screen.fill((0, 0, 0))
         render()
