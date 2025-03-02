f687d9b1c7d32dae0d8e9b2b06471dd73b3d8178
Lvumbets <leva-lev2010@mail.ru>
2025-03-02 17:40:08 +0300

    up/down/right/left 3 classwork task

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ clock = pygame.time.Clock()
 
 delta = 0.005
 theme = 'light'
+min_max_coords = ((37.28, 55.46), (37.32, 55.5))
+coords = [37.3, 55.48]
+
 
 def get_response():
-    global delta
+    global delta, coords
     map_params = {
-        "ll": "37.3,55.48",
+        "ll": ','.join(map(str, coords)),
         "theme": theme,
         "spn": ','.join(map(str, [delta, delta])),
         "apikey": 'a235da75-91e6-4389-8a53-60346aa1414e',
@@ -38,8 +41,8 @@ def render():
 
 
 def run():
-    global delta
-    global theme
+    global delta, theme, coords, min_max_coords
+
     running = True
     while running:  # запуск основного цикла
         for event in pygame.event.get():
@@ -47,14 +50,26 @@ def run():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    theme = 'light'
-                if event.key == pygame.K_2:
-                    theme = 'dark'
-                if event.key == pygame.K_PAGEUP and delta < 3:
-                    delta += 0.1
-                if event.key == pygame.K_PAGEDOWN and delta - 0.1 > 0:
-                    delta -= 0.1
+                if event.key in [pygame.K_1, pygame.K_2]:
+                    if event.key == pygame.K_1:
+                        theme = 'light'
+                    if event.key == pygame.K_2:
+                        theme = 'dark'
+                if event.key in [pygame.K_PAGEUP, pygame.K_PAGEDOWN]:
+                    if event.key == pygame.K_PAGEUP and delta < 3:
+                        delta += 0.1
+                    if event.key == pygame.K_PAGEDOWN and delta - 0.1 > 0:
+                        delta -= 0.1
+                if event.key in [pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT]:
+                    # границы обзора
+                    if event.key == pygame.K_UP and coords[1] < min_max_coords[1][1]:
+                        coords[1] += 0.1*delta
+                    if event.key == pygame.K_DOWN and coords[1] > min_max_coords[0][1]:
+                        coords[1] -= 0.1*delta
+                    if event.key == pygame.K_RIGHT and coords[0] < min_max_coords[1][0]:
+                        coords[0] += 0.1*delta
+                    if event.key == pygame.K_LEFT and coords[0] > min_max_coords[0][0]:
+                        coords[0] -= 0.1*delta
 
         # отображение ресурсов на экране
         screen.fill((0, 0, 0))
